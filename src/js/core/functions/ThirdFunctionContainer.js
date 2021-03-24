1feba585588b8dd9cc8a9c4fd3f8f1c2cbbe3b27
Maxim Monakhov <MaMoDe12@yandex.com>
2021-03-24 13:05:01 +0300

    Some untracked files added to git.

---
+++ src/js/core/functions/ThirdFunctionContainer.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ThirdFunctionContainer = void 0;
+/**
+ *
+ */
+class ThirdFunctionContainer {
+    getFunctionRepresentation() {
+        return "third function MMm";
+    }
+    calc(x) {
+        return 0;
+    }
+    calcFirstDerivative(x) {
+        return 0;
+    }
+    calcSecondDerivative(x) {
+        return 0;
+    }
+    calcFI(x) {
+        return 0;
+    }
+}
+exports.ThirdFunctionContainer = ThirdFunctionContainer;
+//# sourceMappingURL=ThirdFunctionContainer.js.map
